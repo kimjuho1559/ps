9079e96c4866ac135a94bf8682f44d8ea860708e
juhokim <line1559@naver.com>
2025-04-08 14:05:52 +0900

    p25416 bfs 빠른 길 탐색

--- p9020/src/Main.java
+++ p9020/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i <T ; i++) {
+            int n = Integer.parseInt(br.readLine());
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                if (prime(n, i)) {
+                    
+                }
+            }
+        }
+    }
+    static boolean prime(int n) {
+        for (int i = 2; i < (int)Math.sqrt(n)+1; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
